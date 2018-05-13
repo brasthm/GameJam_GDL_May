5ebfc1208b76cd8637cf71475c1e67dd2014a9ac
Thomas Ferrand <a.tom.hic3@gmail.com>
2018-05-13 05:47:53 +0200

    Transition basique

--- src/GameSwitcher.cpp
+++ src/GameSwitcher.cpp
@@ -9,6 +9,8 @@ std::unique_ptr<Screen> GameSwitcher::execute()
     
     bool continueGame = true;
     
+    sf::Sprite gameScreen;
+    
     while(window_.isOpen())
     {
         sf::Event event;
@@ -21,17 +23,25 @@ std::unique_ptr<Screen> GameSwitcher::execute()
         
         if(gameClock.getElapsedTime() > sf::seconds(10) || !continueGame) 
         {
-            gameClock.restart();
 			//HACK désactivation temporaire
-            currentGame_ = std::move(randomGame(currentGame_));
+            auto result = transition(randomGame(currentGame_));
+            if(result)
+                return std::move(*result);
+            gameClock.restart();
             frameClock.restart();
             continueGame = true;
         }
         else
         {
-            window_.clear();
+            renderT_.clear();
             continueGame = currentGame_->computeFrame(frameClock.restart(), score_);
             currentGame_->drawState();
+            renderT_.display();
+            
+            gameScreen.setTexture(renderT_.getTexture());
+            
+            window_.clear();
+            window_.draw(gameScreen);
             window_.display();
         }
         
@@ -47,7 +57,7 @@ std::unique_ptr<Game> GameSwitcher::randomGame()
     static std::default_random_engine gen(rd());
     std::uniform_int_distribution<size_t> distrib(0, game_list.size()-1);
     auto& game_instanciator = *game_list[distrib(gen)];
-    return game_instanciator(window_);
+    return game_instanciator(renderT_);
 }
 
 std::unique_ptr<Game> GameSwitcher::randomGame(const std::unique_ptr<Game>& previous)
@@ -59,4 +69,78 @@ std::unique_ptr<Game> GameSwitcher::randomGame(const std::unique_ptr<Game>& prev
     } while(typeid(*ptr) == typeid(*previous));
     
     return ptr;
+}
+
+std::optional<std::unique_ptr<Screen>> GameSwitcher::transition(std::unique_ptr<Game> nextGame)
+{
+    sf::Time progression;
+    sf::Time openTime = sf::milliseconds(250);
+    sf::Time closeTime = sf::milliseconds(250);
+    
+    sf::Clock frameClock;
+    
+    while(window_.isOpen())
+    {
+        sf::Event event;
+        while(window_.pollEvent(event))
+        {
+            auto next = gestionEvent(event);
+            if(next)
+                return std::move(*next);
+        }
+        
+        progression += frameClock.restart();
+        
+        if(progression < closeTime) {
+            float ratio = progression.asSeconds() / closeTime.asSeconds();
+            sf::Sprite lastGameScreen;
+            
+            renderT_.clear();
+            currentGame_->drawState();
+            renderT_.display();
+            
+            lastGameScreen.setTexture(renderT_.getTexture());
+            
+            sf::RectangleShape rectL({400*ratio, 600});
+            sf::RectangleShape rectR({400*ratio, 600});
+            rectR.setPosition({800- 400*ratio, 0});
+            rectL.setFillColor(sf::Color::Black);
+            rectR.setFillColor(sf::Color::Black);
+            
+            window_.clear();
+            window_.draw(lastGameScreen);
+            window_.draw(rectL);
+            window_.draw(rectR);
+            window_.display();
+        }
+        else if(progression < closeTime+openTime) {
+            float ratio = (progression-closeTime).asSeconds() / openTime.asSeconds();
+            sf::Sprite lastGameScreen;
+
+            renderT_.clear();
+            nextGame->drawState();
+            renderT_.display();
+
+            lastGameScreen.setTexture(renderT_.getTexture());
+
+            sf::RectangleShape rectL({400*(1-ratio), 600});
+            sf::RectangleShape rectR({400*(1-ratio), 600});
+            rectR.setPosition({800- 400*(1-ratio), 0});
+            rectL.setFillColor(sf::Color::Black);
+            rectR.setFillColor(sf::Color::Black);
+
+            window_.clear();
+            window_.draw(lastGameScreen);
+            window_.draw(rectL);
+            window_.draw(rectR);
+            window_.display();
+        }
+        else
+        {
+            currentGame_ = std::move(nextGame);
+            return std::nullopt;
+        }
+    }
+
+    return std::nullopt;
 }
--- src/Games/Games.cpp
+++ src/Games/Games.cpp
@@ -1,17 +1,17 @@
 #include "Games.hpp"
 
 
-std::vector<std::unique_ptr<instanciator_base<Game, sf::RenderWindow&>>> init_game_list()
+std::vector<std::unique_ptr<instanciator_base<Game, sf::RenderTarget&>>> init_game_list()
 {
-    std::vector<std::unique_ptr<instanciator_base<Game, sf::RenderWindow&>>> gl;
-    //gl.emplace_back(new instanciator<Game, BlueScreen, sf::RenderWindow&>);
-    //gl.emplace_back(new instanciator<Game, RedScreen, sf::RenderWindow&>);
-	gl.emplace_back(new instanciator<Game, Boxhead, sf::RenderWindow&>);
-	/*gl.emplace_back(new instanciator<Game, SpaceInvaders, sf::RenderWindow&>);
-	gl.emplace_back(new instanciator<Game, Pacman, sf::RenderWindow&>);
-	gl.emplace_back(new instanciator<Game, Snake, sf::RenderWindow&>);
-    gl.emplace_back(new instanciator<Game, BananaHero, sf::RenderWindow&>);
-    gl.emplace_back(new instanciator<Game, Tetris, sf::RenderWindow&>);
-*/
+    std::vector<std::unique_ptr<instanciator_base<Game, sf::RenderTarget&>>> gl;
+    //gl.emplace_back(new instanciator<Game, BlueScreen, sf::RenderTarget&>);
+    //gl.emplace_back(new instanciator<Game, RedScreen, sf::RenderTarget&>);
+	gl.emplace_back(new instanciator<Game, Boxhead, sf::RenderTarget&>);
+	//gl.emplace_back(new instanciator<Game, SpaceInvaders, sf::RenderTarget&>);
+	gl.emplace_back(new instanciator<Game, Pacman, sf::RenderTarget&>);
+	gl.emplace_back(new instanciator<Game, Snake, sf::RenderTarget&>);
+    gl.emplace_back(new instanciator<Game, BananaHero, sf::RenderTarget&>);
+    //gl.emplace_back(new instanciator<Game, Tetris, sf::RenderTarget&>);
+
     return gl;
 };
